d670536ecb82551b4f88047c311f39f9941d7d6b
lolkitty <lelcetz@gmail.com>
2016-05-27 20:53:57 -0700

    addfact.py
    dunno lol

---
+++ addfact.py
@@ -0,0 +1,16 @@
+n = int(input(' Give me a number: ')) # gets two numbers
+w = int(input(' Give me another a number: '))
+
+def addfac(x,y): # defines function
+    result = 0 # defines result
+    list = range(x,y+1) # generates list includng x and y.
+    #list = range(x,y)
+    #list.append(y)
+    for i in range(0,len(list)): # generates for loop from 0 to the length of the list.
+        result = result + list[i] # adds all the indexes together
+    return str(result) #returns result as string.
+
+#explains and prints result.
+print ("This is all the numbers from " + str(n) + " to " + str(w) + " added up INCLUDING the two numbers. So, 1 and 3 =1 + 2 + 3" )
+print (addfac(n,w))
+
