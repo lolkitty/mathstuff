442b2ae960ac2db402e9a645c4419ccfc75474f2
lolkitty <lelcetz@gmail.com>
2016-05-29 09:06:26 -0700

    added functions
    wee

--- eu1modif.py
+++ eu1modif.py
@@ -2,3 +2,12 @@
 x  = input("Enter a number: ")
 y  = input("Enter a number: ")
 n  = input("Enter a number: ")
+
+num = range(0,n)
+num.append(i)
+for i in range(0,len(num)):
+  if num[i]%x != 0:
+    del num[i]
+  elif num[i]%y != 0:
+    del num[i]
+print(num)
