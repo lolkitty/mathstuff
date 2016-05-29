13238cc6ff4fc620427d49c48a99e7b6e1a829c9
lolkitty <lelcetz@gmail.com>
2016-05-29 09:07:20 -0700

    Fixed bug that made it not work
    cant believe that the entire program didnt work because of 4 words

--- eu1modif.py
+++ eu1modif.py
@@ -3,7 +3,7 @@ x  = input("Enter a number: ")
 y  = input("Enter a number: ")
 n  = input("Enter a number: ")
 
-num = range(0,n)
+num = list(range(0,n))
 num.append(i)
 for i in range(0,len(num)):
   if num[i]%x != 0:
